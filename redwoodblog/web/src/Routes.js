952e01499a44592dd43c0e535c3387f1f6d660af
michael mentele <michaelrmentele@gmail.com>
2022-02-22 14:18:26 -0600

    saving data

--- redwoodblog/web/src/Routes.js
+++ redwoodblog/web/src/Routes.js
@@ -6,22 +6,21 @@ import BlogLayout from 'src/layouts/BlogLayout'
 
 const Routes = () => {
   return (
-<Router>
-  <Route path="/contact" page={ContactPage} name="contact" />
-  <Set wrap={PostsLayout}>
-    <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
-    <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
-    <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
-    <Route path="/posts" page={PostPostsPage} name="posts" />
-    
-  </Set>
-  <Set wrap={BlogLayout}>
-<Route path="/article/{id:Int}" page={ArticlePage} name="article" />
-    <Route path="/about" page={AboutPage} name="about" />
-    <Route path="/" page={HomePage} name="home" />
-  </Set>
-  <Route notfound page={NotFoundPage} />
-</Router>
+    <Router>
+      <Set wrap={PostsLayout}>
+        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
+        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
+        <Route path="/posts" page={PostPostsPage} name="posts" />
+      </Set>
+      <Set wrap={BlogLayout}>
+        <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
+        <Route path="/contact" page={ContactPage} name="contact" />
+        <Route path="/about" page={AboutPage} name="about" />
+        <Route path="/" page={HomePage} name="home" />
+      </Set>
+      <Route notfound page={NotFoundPage} />
+    </Router>
   )
 }
 
--- redwoodblog/web/src/pages/ContactPage/ContactPage.js
+++ redwoodblog/web/src/pages/ContactPage/ContactPage.js
@@ -1,19 +1,63 @@
-import { Link, routes } from '@redwoodjs/router'
+// web/src/pages/ContactPage/ContactPage.js
+
 import { MetaTags } from '@redwoodjs/web'
+import {
+  FieldError,
+  Form,
+  Label,
+  TextField,
+  TextAreaField,
+  Submit,
+} from '@redwoodjs/forms'
 
 const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <h1>ContactPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ContactPage/ContactPage.js</code>
-      </p>
-      <p>
-        My default route is named <code>contact</code>, link to me with `
-        <Link to={routes.contact()}>Contact</Link>`
-      </p>
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
+
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+     
+<TextField
+  name="email"
+  validation={{
+    required: true,
+    pattern: {
+      value: /^[^@]+@[^.]+\..+$/,
+    },
+  }}
+  errorClassName="error"
+/>
+        <FieldError name="email" className="error" />
+
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
+        <Submit>Save</Submit>
+      </Form>
     </>
   )
 }
