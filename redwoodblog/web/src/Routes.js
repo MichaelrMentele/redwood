71bec35fc48bbeb731522e32ef4ce8d9986470d0
michael mentele <michaelrmentele@gmail.com>
2022-02-22 14:10:49 -0600

    start building a form section

--- redwoodblog/web/src/Routes.js
+++ redwoodblog/web/src/Routes.js
@@ -6,19 +6,22 @@ import BlogLayout from 'src/layouts/BlogLayout'
 
 const Routes = () => {
   return (
-    <Router>
-      <Set wrap={PostsLayout}>
-        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
-        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
-        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
-        <Route path="/posts" page={PostPostsPage} name="posts" />
-      </Set>
-      <Set wrap={BlogLayout}>
-        <Route path="/about" page={AboutPage} name="about" />
-        <Route path="/" page={HomePage} name="home" />
-      </Set>
-      <Route notfound page={NotFoundPage} />
-    </Router>
+<Router>
+  <Route path="/contact" page={ContactPage} name="contact" />
+  <Set wrap={PostsLayout}>
+    <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
+    <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+    <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
+    <Route path="/posts" page={PostPostsPage} name="posts" />
+    
+  </Set>
+  <Set wrap={BlogLayout}>
+<Route path="/article/{id:Int}" page={ArticlePage} name="article" />
+    <Route path="/about" page={AboutPage} name="about" />
+    <Route path="/" page={HomePage} name="home" />
+  </Set>
+  <Route notfound page={NotFoundPage} />
+</Router>
   )
 }
 
